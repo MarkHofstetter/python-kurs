d1d78b7522321a0612d326fe81951d635e0ccdcd
Mark Hofstetter <mark@hofstetter.at>
2017-01-23 10:57:03 +0100

    kurze einfuehrung in variablen

---
+++ variablen.py
@@ -0,0 +1,9 @@
+a = 2
+z = "2"
+b = "Hallo Welt"
+# a = "Zwei"
+c = str(a)
+print(a, b) ## einfache Ausgabe OHNE Verkettung
+# print(a + b) ## operand auf unterschiedliche Typen funktioniert nicht
+print(str(a) + b) # oder
+print(c + b)
